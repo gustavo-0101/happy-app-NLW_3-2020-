fbd6a637453c1038da0e63cd8d11762a374232c2
gustavo-0101 <guga-rd@hotmail.com>
2020-10-16 14:45:47 -0300

    map finished

--- src/pages/OrphanegesMap.tsx
+++ src/pages/OrphanegesMap.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import mapMarkering from "../images/map-marker.svg";
 
 import "../styles/pages/orphanages-map.css";
 import "leaflet/dist/leaflet.css";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkering,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 function OrphanagesMap() {
   return (
@@ -31,9 +40,23 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker icon={mapIcon} position={[-29.3622556, -50.8162796]}>
+          <Popup
+            closeButton={false}
+            minWidth={248}
+            maxWidth={248}
+            className="map-popup"
+          >
+            Umbrella Academy
+            <Link to="orphanages/1">
+              <FiArrowRight size={32} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
